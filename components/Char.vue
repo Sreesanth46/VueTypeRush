5e3f056c238e628d6f0e15eda6a9e9c30893a624
Sreesanth <sreesanth46@gmail.com>
2024-07-28 02:35:41 +0530

    feat: component to show each character

---
+++ components/Char.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, toRef } from "vue";
+const props = defineProps<{
+  actual: string;
+  typed?: string;
+}>();
+
+const typed = toRef(props, "typed");
+
+const textColor = computed(() => {
+  if (!typed.value) return "text-blue-500";
+  if (props.actual.localeCompare(typed.value) === 0) return "text-green-500";
+  return "text-red-500";
+});
+</script>
+<template>
+  <span :class="textColor">
+    {{ actual }}
+  </span>
+</template>
