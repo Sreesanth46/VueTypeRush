e9a1ba216382937af7ba445c61e0c2aa449ca120
Sreesanth <sreesanth46@gmail.com>
2024-07-28 19:52:58 +0530

    feat: emit mistakes

--- components/Char.vue
+++ components/Char.vue
@@ -5,11 +5,17 @@ const props = defineProps<{
   typed?: string;
 }>();
 
+const emit = defineEmits<{
+  mistake: [];
+}>();
+
 const typed = toRef(props, "typed");
 
 const textColor = computed(() => {
   if (!typed.value) return "text-blue-500";
   if (props.actual.localeCompare(typed.value) === 0) return "text-green-500";
+
+  emit("mistake");
   return "text-red-500";
 });
 </script>
