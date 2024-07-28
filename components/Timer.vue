1ec9260e8887e2c2c2d9487a3c5fa4fe49cbc20f
Sreesanth <sreesanth46@gmail.com>
2024-07-28 20:35:11 +0530

    style: updated timer styles

--- components/Timer.vue
+++ components/Timer.vue
@@ -13,17 +13,35 @@ const { counter, pause } = useInterval(1000, {
 });
 
 const countDown = computed(() => {
-  if (!props.start && counter.value !== props.timer + 1) return props.timer;
+  if (!props.start && counter.value !== props.timer + 1) {
+    return {
+      class: "text-white",
+      timer: props.timer,
+    };
+  }
 
   const remainingTime = props.timer - counter.value;
+
   if (remainingTime < 0) {
     pause();
     emit("timeUp");
-    return "Time is up!";
-  } else return remainingTime;
+    return {
+      class: "text-red-500 font-bold",
+      timer: "Time is up!",
+    };
+  } else {
+    let styleClass = "text-white";
+    if (remainingTime <= 10) styleClass = "text-red-400 animate-ping";
+    return {
+      class: styleClass,
+      timer: remainingTime,
+    };
+  }
 });
 </script>
 
 <template>
-  <p>{{ countDown }}</p>
+  <p :class="countDown.class">
+    {{ countDown.timer }}
+  </p>
 </template>
